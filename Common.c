c7c43c7db5b49b543de45774f2054a7cfa30c1ad
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2019-03-29 13:30:56 +0300

    Add files via upload

---
+++ Common.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int d;
+	printf("Hello guys,my deals so nice");
+	scanf ("%d",&d);
+	for(d=0;;d++)
+	printf("Botya_loh\n");
+	return 0;
+}
